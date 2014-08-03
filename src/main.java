db9e866ed052f00a096b82c99b9b7d8536861d01
Tyler Wong <ty-w@live.com>
2014-08-03 00:58:29 -0700

    Initial GUI commit
    Added the GUI to the program: a simple panel with email and password
    text fields.

--- src/main.java
+++ src/main.java
@@ -1,9 +1,281 @@
+/*
+ * BingBot
+ * Automates the eaerning of points for Bing rewards.
+ * Made by Tyler Wong
+ */
 
-public class main {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public static void main(String[] args) {
-		
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
 
-	}
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
+public class main extends JFrame {
+    public main() {
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // Load GUI                        
+    private void initComponents() {
+
+        accountPanel = new JPanel();
+        // initialize load variables
+        Properties prop = new Properties();
+        InputStream input = null;
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("}@d[HZPn{;$&1c81oPui");
+        OutputStream output = null;
+        email1 = new JTextField();
+        email2 = new JTextField();
+        email3 = new JTextField();
+        email4 = new JTextField();
+        email5 = new JTextField();
+        password1 = new JPasswordField();
+        password2 = new JPasswordField();
+        password3 = new JPasswordField();
+        password4 = new JPasswordField();
+        password5 = new JPasswordField();
+        emailLabel = new JLabel();
+        passwordLabel = new JLabel();
+        saveButton = new JButton();
+        startButton = new JButton();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("BingBot");
+
+        accountPanel.setBorder(BorderFactory.createTitledBorder(null, "Account Information", TitledBorder.CENTER, TitledBorder.DEFAULT_POSITION));
+
+        // load logins
+        try {
+            String appdata = System.getenv("APPDATA");      // gets the path to the local roaming profile
+            input = new FileInputStream(appdata + "/BingBot/config.properties");
+            prop.load(input);   // load the file
+
+            // sets the account information in the UI
+            email1.setText(prop.getProperty("1.email"));
+            password1.setText(encryptor.decrypt(prop.getProperty("1.password")));
+
+            email2.setText(prop.getProperty("2.email"));
+            password2.setText(encryptor.decrypt(prop.getProperty("2.password")));
+
+            email3.setText(prop.getProperty("3.email"));
+            password3.setText(encryptor.decrypt(prop.getProperty("3.password")));
+
+            email4.setText(prop.getProperty("4.email"));
+            password4.setText(encryptor.decrypt(prop.getProperty("4.password")));
+
+            email5.setText(prop.getProperty("5.email"));
+            password5.setText(encryptor.decrypt(prop.getProperty("5.password")));
+        }
+        catch(IOException e) {
+            String appdata = System.getenv("APPDATA");      // gets the path to the local roaming profile
+            new File(appdata + "/BingBot").mkdir();   // creates a folder to put the properties file in
+            try {
+                output = new FileOutputStream(appdata + "/BingBot/config.properties");
+            }
+            catch(Exception e2) { }
+        }
+        finally {   // clean up
+            if(input != null) {
+                try {
+                    input.close();
+                }
+                catch(IOException e) {}
+            }
+        }
+
+        emailLabel.setText("Email");
+
+        passwordLabel.setText("Password");
+
+        saveButton.setText("Save");
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                saveButtonActionPerformed(evt);
+            }
+        });
+
+        startButton.setText("Start");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        GroupLayout accountPanelLayout = new GroupLayout(accountPanel);
+        accountPanel.setLayout(accountPanelLayout);
+        accountPanelLayout.setHorizontalGroup(
+            accountPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(accountPanelLayout.createSequentialGroup()
+                .addGap(63, 63, 63)
+                .addComponent(emailLabel)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(passwordLabel)
+                .addGap(66, 66, 66))
+            .addGroup(accountPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addComponent(saveButton)
+                    .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                        .addComponent(email2)
+                        .addComponent(email3)
+                        .addComponent(email4)
+                        .addComponent(email5, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
+                        .addComponent(email1)))
+                .addGap(18, 18, 18)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(password1, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password2, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password3, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password4, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password5, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(startButton))
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+        accountPanelLayout.setVerticalGroup(
+            accountPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, accountPanelLayout.createSequentialGroup()
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(emailLabel)
+                    .addComponent(passwordLabel))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(email1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(email2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(email3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(email4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(email5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(password5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(accountPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(saveButton)
+                    .addComponent(startButton)))
+        );
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(accountPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(accountPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>                        
+
+    private void saveButtonActionPerformed(ActionEvent evt) {                                           
+        Properties prop = new Properties();
+        OutputStream output = null;
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("}@d[HZPn{;$&1c81oPui");
+        
+        try {
+            String appdata = System.getenv("APPDATA");      // gets the path to the local roaming profile
+            new File(appdata + "/BingBot").mkdir();   // creates a folder to put the properties file in
+            output = new FileOutputStream(appdata + "/BingBot/config.properties");
+            
+            prop.setProperty("1.email", email1.getText().toLowerCase().trim());
+            prop.setProperty("2.email", email2.getText().toLowerCase().trim());
+            prop.setProperty("3.email", email3.getText().toLowerCase().trim());
+            prop.setProperty("4.email", email4.getText().toLowerCase().trim());
+            prop.setProperty("5.email", email5.getText().toLowerCase().trim());
+
+            prop.setProperty("1.password", encryptor.encrypt(password1.getText().trim()));
+            prop.setProperty("2.password", encryptor.encrypt(password2.getText().trim()));
+            prop.setProperty("3.password", encryptor.encrypt(password3.getText().trim()));
+            prop.setProperty("4.password", encryptor.encrypt(password4.getText().trim()));
+            prop.setProperty("5.password", encryptor.encrypt(password5.getText().trim()));
+            
+            prop.store(output, null);
+        }
+        catch(IOException e) {
+            JOptionPane.showMessageDialog(null, "File could not be saved.", "File could not be saved.", JOptionPane.ERROR_MESSAGE);}
+        finally {   // clean up
+            if(output != null) {
+                try {
+                    output.close();
+                }
+                catch(IOException e) {}
+            }
+        }
+    }                                          
+
+    private void startButtonActionPerformed(ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    }                                           
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new main().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify                     
+    private JPanel accountPanel;
+    private JTextField email1;
+    private JTextField email2;
+    private JTextField email3;
+    private JTextField email4;
+    private JTextField email5;
+    private JLabel emailLabel;
+    private JPasswordField password1;
+    private JPasswordField password2;
+    private JPasswordField password3;
+    private JPasswordField password4;
+    private JPasswordField password5;
+    private JLabel passwordLabel;
+    private JButton saveButton;
+    private JButton startButton;
+    // End of variables declaration                   
 }
