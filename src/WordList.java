bb56b1b6efd82f4134672645ae9927dec72e65e3
Tyler Wong <ty-w@live.com>
2014-08-09 01:56:06 -0700

    Wordlist not checks current list
    The wordlist.java now checks the current word list before adding a word
    to the list so the list should slowly become larger over time.

--- src/WordList.java
+++ src/WordList.java
@@ -1,6 +1,10 @@
-import java.util.HashMap;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.*;
@@ -9,6 +13,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WordList {
     
+	// holds the words. using a treeSet for fast searching as it goes by natural order
+	SortedSet<String> wordSet = new TreeSet<String>();
+	
+	private String filePath = (System.getProperty("user.dir") + "\\WordList.txt");
     // declare variables for selenium
     private WebDriver driver;
     private String wikiUrl;
@@ -29,6 +37,19 @@ public class WordList {
         wikiUrl = "http://en.wikipedia.org/wiki/Wikipedia:5000";		//This list updates every Sunday morning (UTC), aggregating data from the 7 days preceeding 11:59PM Saturday.
         aolUrl = "http://search.aol.com/aol/trends";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        
+        // load current word list
+        try {
+        	Scanner s = new Scanner(new File(filePath));
+        	s.useDelimiter("\n");
+        	while(s.hasNext()) {
+        		wordSet.add(s.next().trim());
+        	}
+        	s.close();
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
     }
     
     @Test
@@ -41,7 +62,11 @@ public class WordList {
     	
     	for(WebElement tr : tr_collection) {
     		List<WebElement> td_collection = tr.findElements(By.tagName("td"));
-    		System.out.println(td_collection.get(1).getText());
+    		
+    		String word = td_collection.get(1).getText().trim();
+    		if(!wordSet.contains(word)) {
+    			wordSet.add(word);
+    		}
     	}
     	
     	// get 50 more words from aol top daily searches
@@ -51,22 +76,29 @@ public class WordList {
     	List<WebElement> li_collection = list.findElements(By.tagName("li"));
     	
     	for(WebElement li : li_collection) {
-    		System.out.println(li.getText());
+    		String word = li.getText().trim();
+    		
+    		if(!wordSet.contains(word)) {
+    			wordSet.add(word);
+    		}
     	}
     }
     
     @After
     public void tearDown() {
-        //driver.quit();
-    }
+        driver.quit();
         
-    private boolean isElementPresent(By by) {
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        // write to text file
         try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+			FileWriter writer = new FileWriter(filePath);
+			
+			for(String word : wordSet) {
+				writer.append(word + "\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
